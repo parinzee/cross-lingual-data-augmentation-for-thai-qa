5779d4328efe1ab637e620909edb916e3c8932b1
Parinthapat Pengpun <parinzee@protonmail.com>
2023-05-21 16:47:00 +0700

    💥 Remove AI21 LLM Augments for OpenAI API instead
    Signed-off-by: Parinthapat Pengpun <parinzee@protonmail.com>

--- 03_Augment_LLM.py
+++ 03_Augment_LLM.py
@@ -5,17 +5,43 @@
 import os
 import threading
 
-import ai21
-from ai21.errors import ServerError
+import openai
+from tenacity import (
+    retry,
+    stop_after_attempt,
+    wait_random_exponential,
+)
 
 import pandas as pd
 import tqdm.auto as tqdm
-from time import sleep
+from rich import print
 
 # Load api key
-api_key = os.environ['AI21_API_KEY']
-ai21.api_key = api_key
+api_key = os.environ['OPENAI_API_KEY']
+openai.api_key = api_key
+
+# Load data
 df = pd.read_csv('data/02_backtranslate_english.csv')
+
+# AI Constants
+# These original messages are generated from the same model, however we include them inside the chat to make sure the model returns in the correct format.
+model = "gpt-3.5-turbo-0301"
+example_grammar_error_correction_messages = [
+    {'role': 'system', 'content': "You are a highly skilled language model AI. Your task is to evaluate each sentence from a given list and correct its grammar. Even if a sentence is not clear or intelligible, do your best to make a grammatical correction, even if you have to make assumptions about the intended meaning. If the sentence is grammatically correct, do not change it. Your output should be presented with ONLY each corrected sentence written on a new line."},
+    {"role": "user", "content": "Phatthira Sarutpong Phokin What is the date of birth?\nPhatthira Sarutpong Phokin What are you playing?\nWhat is the career of Phatthira Teerathiyapong Phokin?\nPhatthira Sarutpong Phokin Graduated from what country?\nWho is Cleopatra's father?"},
+    {"role": "assistant", "content": "What is Phatthira Sarutpong Phokin's date of birth?\nWhat are you playing, Phatthira Sarutpong Phokin?\nWhat is Phatthira Teerathiyapong Phokin's career?\nPhatthira Sarutpong Phokin graduated from what country?\nWho is Cleopatra's father?"},
+]
+
+example_paraphrase_messages = [
+    {"role": "system", "content": "You are a highly skilled language model AI. Your task is to perform two specific actions on a given list of sentences. First, evaluate each sentence and make sure it's grammatically correct. If a sentence is not grammatically correct, fix it. Then, ALWAYS paraphrase each sentence while maintaining its original meaning.  Your output should be presented with ONLY each paraphrased sentence written on a new line."},
+    {"role": "user", "content": "Phatthira Sarutpong Phokin What is the date of birth?\nPhatthira Sarutpong Phokin What are you playing?\nWhat is the career of Phatthira Teerathiyapong Phokin?\nPhatthira Sarutpong Phokin Graduated from what country?\nWho is Cleopatra's father?"},
+    {"role": "assistant", "content": "Phatthira Sarutpong Phokin, what is your date of birth?\nWhat are you playing, Phatthira Sarutpong Phokin?\nWhat is Phatthira Teerathiyapong Phokin's career?\nFrom which country did Phatthira Sarutpong Phokin graduate?\nWho is the father of Cleopatra?"}
+]
+
+# Add exponential backoff to completion
+@retry(wait=wait_random_exponential(min=1, max=60), stop=stop_after_attempt(6))
+def completion_with_backoff(**kwargs):
+    return openai.ChatCompletion.create(**kwargs)
 
 # Grammar Error Correction
 def grammar_error_correction():
@@ -23,39 +49,49 @@ def grammar_error_correction():
     if os.path.exists('data/03_augment_llm_gec.csv'):
         data = pd.read_csv('data/03_augment_llm_gec.csv').to_dict('records')
         completed_ids = set([x["id"] for x in data])
-    
     else:
         data = []
         completed_ids = set()
     
-    for i, row in tqdm.tqdm(df.iterrows(), total=len(df), desc="Grammar Error Correction"):
-        if row['id'] in completed_ids:
+    # Loop through each row in the dataframe using batches of 25
+    for i in tqdm.tqdm(range(0, len(df), 25), desc="Correcting grammar"):
+        # Get the next 25 rows
+        batch = df.iloc[i:i+25]
+        batch = batch[~batch["id"].isin(completed_ids)]
+        if len(batch) == 0:
             continue
-        
-        # Grammar correction
-        try:
-            resp = ai21.GEC.execute(text=row["en_aug"])
-            corrections = resp["corrections"]
 
-            # Apply corrections
-            corrected_text = row["en_aug"]
-            for curr_correction in reversed(corrections):
-                corrected_text = corrected_text[:curr_correction["startIndex"]] + curr_correction['suggestion'] + corrected_text[curr_correction["endIndex"]:]
-        except ServerError as e:
-            print(e)
-            print(f"Error on id: {row['id']}")
-            print(f"Error on text: {row['en_aug']}")
-            print("Apending original text instead")
-            corrected_text = row["en_aug"]
+        to_correct = "\n".join(batch["en_aug"].tolist())
+        attempt = 0
+        while attempt < 3:
+            try:
+                response = completion_with_backoff(
+                    model=model,
+                    messages=[
+                        *example_grammar_error_correction_messages, 
+                        {"role": "user", "content": to_correct} 
+                    ],
+                )
+                raw_corrected = response['choices'][0]['message']['content']
+                corrected = raw_corrected.split("\n")
+                assert len(corrected) == len(batch)
+                break
+            except AssertionError:
+                print(f"Error: {raw_corrected}")
+                print(f"Error: {batch}")
+                print(f"Attempt {attempt+1} of 3")
+                if attempt == 3:
+                    raise
         
-        # Append to data
-        data.append({
-            "id": row['id'],
-            "en_llm_grammar_aug": corrected_text
-        })
+        # Add to data
+        for idx, corrected_sentence in enumerate(corrected):
+            data.append({
+                "id": batch.iloc[idx]["id"],
+                "en_llm_gec_aug": corrected_sentence,
+            })
         
+        # Save data
         pd.DataFrame(data).to_csv('data/03_augment_llm_gec.csv', index=False)
-        sleep(2.3)
 
 # Paraphrase
 def paraphrase():
@@ -67,41 +103,47 @@ def paraphrase():
         data = []
         completed_ids = set()
     
-    for i, row in tqdm.tqdm(df.iterrows(), total=len(df), desc="Paraphrase"):
-        if row['id'] in completed_ids:
+    # Loop through each row in the dataframe using batches of 25
+    for i in tqdm.tqdm(range(0, len(df), 25), desc="Paraphrasing questions"):
+        # Get the next 25 rows
+        batch = df.iloc[i:i+25]
+        batch = batch[~batch["id"].isin(completed_ids)]
+        if len(batch) == 0:
             continue
+
+        to_paraphrase = "\n".join(batch["en_aug"].tolist())
+        attempt = 0
+        while attempt < 3:
+            try:
+                response = completion_with_backoff(
+                    model=model,
+                    messages=[
+                        *example_paraphrase_messages, 
+                        {"role": "user", "content": to_paraphrase} 
+                    ],
+                )
+                raw_paraphrases = response['choices'][0]['message']['content']
+                paraphrases = raw_paraphrases.split("\n")
+                assert len(paraphrases) == len(batch)
+                break
+            except AssertionError:
+                print(f"Error: {raw_paraphrases}")
+                print(f"Error: {batch}")
+                print(f"Attempt {attempt+1} of 3")
+                if attempt == 3:
+                    raise
         
-        # Paraphrase
-        resp = ai21.Paraphrase.execute(text=row["en_aug"])
-        paraphrases = resp["suggestions"]
-        for paraphrase in paraphrases:
-            # Append to data
+        # Add to data
+        for idx, paraphrased in enumerate(paraphrases):
             data.append({
-                "id": row['id'],
-                "en_llm_paraphrase_aug": paraphrase["text"]
+                "id": batch.iloc[idx]["id"],
+                "en_llm_paraphrase_aug": paraphrased
             })
-        
         pd.DataFrame(data).to_csv('data/03_augment_llm_paraphrase.csv', index=False)
-        sleep(1.5)
-    
-
-def run_grammar_error_correction():
-    try:
-        grammar_error_correction()
-    except:
-        # Rerun once more
-        grammar_error_correction()
-
-def run_paraphrase():
-    try:
-        paraphrase()
-    except:
-        # Rerun once more
-        paraphrase()
 
 if __name__ == "__main__":
-    thread_1 = threading.Thread(target=run_grammar_error_correction) 
-    thread_2 = threading.Thread(target=run_paraphrase)
+    thread_1 = threading.Thread(target=grammar_error_correction) 
+    thread_2 = threading.Thread(target=paraphrase)
     
     # Start both threads
     thread_1.start()
